e4c067a69ae11075deaee7d40489ea227b073afc
Guillaume <inoxsdev@gmail.com>
2022-06-15 08:35:48 +0200

    arrange import

--- pages/_app.js
+++ pages/_app.js
@@ -10,28 +10,6 @@ function MyApp({ Component, pageProps }) {
           useSystemColorMode: false,
         }}
       ></ColorModeProvider>
-      <style jsx global>{`
-        #__next {
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-        }
-        @media only screen and (max-height: 500px) {
-          #__next {
-            height: 100%;
-          }
-        }
-        .chakra-wrap__list {
-          grid-gap: 30px;
-        }
-        main {
-          margin-inline-end: 0 !important;
-          -webkit-margin-end: 0 !important;
-          -webkit-margin-start: 0 !important;
-          min-height: auto !important ;
-          min-width: 0 !important;
-        }
-      `}</style>
 
       <Component {...pageProps} />
     </ChakraProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import {
   useColorMode,
-  Heading,
-  Text,
   Flex,
   HStack,
-  Box,
   Button,
   Link,
   Center,
@@ -13,7 +10,7 @@ import {
 import { Image } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
-import { Fade, ScaleFade, Slide, SlideFade } from "@chakra-ui/react";
+import { Fade } from "@chakra-ui/react";
 import {
   FaSoundcloud,
   FaYoutube,
@@ -21,12 +18,6 @@ import {
   FaFacebook,
 } from "react-icons/fa";
 export default function Index() {
-  const { colorMode } = useColorMode();
-  const colorSecondary = {
-    light: "gray.700",
-    dark: "gray.400",
-  };
-
   return (
     <Container bg={"#1A202C"} maxW="container.2xl" h={"100vh"} w="100%">
       <Head>
@@ -36,6 +27,7 @@ export default function Index() {
         <Center h="500px" color="white">
           <Flex align="center">
             <Image
+              mt={10}
               src="/logo_isi8_vector_white.svg"
               alt="me"
               width="500px"
@@ -43,14 +35,14 @@ export default function Index() {
             />
           </Flex>
         </Center>
-        <Center h="100px">
+        <Center mt={10} h="100px">
           <HStack padding="50px" maxW="100%">
-            <Button colorScheme="white" variant="outline">
+            <Button colorScheme="white">
               <Link href="https://www.instagram.com/isi8official/" isExternal>
                 <Icon as={FaInstagram} w={10} h={10} color={"white"} />
               </Link>
             </Button>
-            <Button colorScheme="white" variant="outline">
+            <Button colorScheme="white">
               <Link
                 href="https://www.facebook.com/ISI-8-101967501917069"
                 isExternal
@@ -58,7 +50,7 @@ export default function Index() {
                 <Icon as={FaFacebook} w={10} h={10} color={"white"} />
               </Link>
             </Button>
-            <Button colorScheme="white" variant="outline">
+            <Button colorScheme="white">
               <Link
                 href="https://www.youtube.com/channel/UCxsLcxMZRSAajpZQNTNsRrw"
                 isExternal
@@ -66,7 +58,7 @@ export default function Index() {
                 <Icon as={FaYoutube} w={10} h={10} color={"white"} />
               </Link>
             </Button>
-            <Button colorScheme="white" variant="outline">
+            <Button colorScheme="white">
               <Link href="https://soundcloud.com/isi8band" isExternal>
                 <Icon as={FaSoundcloud} w={10} h={10} color={"white"} />
               </Link>
